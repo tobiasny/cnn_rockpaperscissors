703c7308d08a4528b025a8aca13d87f8039d5338
tobiasny <tobiasny@stud.ntnu.no>
2019-01-04 15:36:34 +0100

    Updated CNN architecture: 58.75% validation accuracy

--- CNN_RPS.m
+++ CNN_RPS.m
@@ -1,7 +1,27 @@
 
 imds = imageDatastore("/Users/Tobias/Downloads/rps20180219",'IncludeSubfolders',true,'LabelSource','foldernames');
+imds.ReadSize = numpartitions(imds);
+imds.ReadFcn = @(loc)imresize(imread(loc),[227,227]);
+
+% Divide into training and validation data sets
+numTrainFiles = 120;
+[imdsTrain,imdsValidation] = splitEachLabel(imds,numTrainFiles,'randomize');
+
 
 %%
+
+% paper = find(imds.Labels == 'paper', 1);
+% rock = find(imds.Labels == 'rock', 1);
+% scissor = find(imds.Labels == 'scissor', 1);
+% 
+% figure
+% subplot(1,3,1);
+% imshow(readimage(imds,paper))
+% subplot(1,3,2);
+% imshow(readimage(imds,rock))
+% subplot(1,3,3);
+% imshow(readimage(imds,scissor))
+
 
 % # of samples of each label
 %labelCount = countEachLabel(imds)
@@ -10,43 +30,58 @@ imds = imageDatastore("/Users/Tobias/Downloads/rps20180219",'IncludeSubfolders',
 %img = readimage(imds,1);
 %size(img)
 
-% Divide into training and validation data sets
-numTrainFiles = 120;
-[imdsTrain,imdsValidation] = splitEachLabel(imds,numTrainFiles,'randomize');
+%%
+
+%convnet = alexnet;
+%layers = convnet.Layers;
+
 
 % Define CNN architecture
 layers = [
-    imageInputLayer([500 500 3])
-    %{
-    convolution2dLayer(100,8,'Padding','same')
+    imageInputLayer([227 227 3])
+    
+    convolution2dLayer(50,96,'Stride',4)
     batchNormalizationLayer
     reluLayer
     
-    maxPooling2dLayer(2,'Stride',2)
+    maxPooling2dLayer(3,'Stride',2)
     
-    convolution2dLayer(3,16,'Padding','same')
+    convolution2dLayer(20,256,'Stride',1,'Padding',2)
     batchNormalizationLayer
     reluLayer
     
-    maxPooling2dLayer(2,'Stride',2)
+    maxPooling2dLayer(3,'Stride',2)
     
-    convolution2dLayer(3,32,'Padding','same')
+    convolution2dLayer(3,384,'Stride',1,'Padding',1)
     batchNormalizationLayer
     reluLayer
-    %}
+    
+    maxPooling2dLayer(3,'Stride',2)
+    
+    fullyConnectedLayer(4096)
+    reluLayer
+    dropoutLayer(0.5)
+    
     fullyConnectedLayer(3)
     softmaxLayer
     classificationLayer];
 
 % Specify training options
-options = trainingOptions('sgdm', ...
-    'InitialLearnRate',0.01, ...
-    'MaxEpochs',15, ...
-    'Shuffle','every-epoch', ...
-    'ValidationData',imdsValidation, ...
-    'ValidationFrequency',2, ...
-    'Verbose',false, ...
-    'Plots','training-progress');
+% options = trainingOptions('sgdm', ...
+%      'InitialLearnRate',0.1, ...
+%      'MaxEpochs',10, ...
+%      'Shuffle','every-epoch', ...
+%      'ValidationData',imdsValidation, ...
+%      'ValidationFrequency',4, ...
+%      'Verbose',false, ...
+%      'Plots','training-progress');
+
+ options = trainingOptions('sgdm', ...
+     'MaxEpochs',8, ...
+     'ValidationData',imdsValidation, ...
+     'ValidationFrequency',30, ...
+     'Verbose',false, ...
+     'Plots','training-progress');
 
 % Train network
 net = trainNetwork(imdsTrain,layers,options);
